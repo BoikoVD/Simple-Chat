74054cadbad9d5de165948bda1aee3b799f3ea35
BoikoVD <boiko.volodymyr.dm@gmail.com>
2021-10-15 01:57:51 +0300

    Redesign. Height parameters changed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
